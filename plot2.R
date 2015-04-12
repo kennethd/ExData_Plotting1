1cfd1599b93cd5af14e34bd318981fffff4217ac
Kenneth Dombrowski <kenneth@ylayali.net>
2015-04-12 19:35:57 -0400

    stringsAsFactors = FALSE

--- plot2.R
+++ plot2.R
@@ -36,7 +36,7 @@ if (! file.exists(LOCAL_TXT_FILE_NAME)) {
     extractFiles()
 }
 
-d <- read.csv(LOCAL_TXT_FILE_NAME, sep = ";", 
+d <- read.csv(LOCAL_TXT_FILE_NAME, sep = ";", stringsAsFactors = FALSE,
               col.names = c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", 
                             "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 
